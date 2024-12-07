256c2bcd1ae15523a79ca7d51a071cbe3d80a925
Udin <adefakhrudin260304@gmail.com>
2024-12-07 17:00:18 +0700

    update nama database

--- src/services/getAllData.js
+++ src/services/getAllData.js
@@ -2,7 +2,7 @@ const { Firestore } = require('@google-cloud/firestore');
 
 async function getAllData() {
     const db = new Firestore();
-    const predictCollection = db.collection('prediksi');
+    const predictCollection = db.collection('predictions');
     
     const allData = await predictCollection.get();
     return allData;
